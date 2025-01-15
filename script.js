cfd3d2eae1fa5ab9e71b25f755fbf26abe570f17
LorenaBarr <karenlbarrera@gmail.com>
2025-01-14 19:42:13 -0500

    variables de css para cambio de tema oscuro/claro

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function calculate() {
     document.getElementById('display').value = 'Error';
   }
 }
+
+
+const toggleTheme = () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+};
+
+document.querySelector('#theme-toggle').addEventListener('click', toggleTheme);
